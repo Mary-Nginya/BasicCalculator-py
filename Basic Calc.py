a857e5b631eea9f757acaa428d1ba03728db9adb
Mary-Nginya <mail.marynginya@gmail.com>
2025-04-06 14:14:15 +0300

    first commit tested.

---
+++ Basic Calc.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1 - num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    result = num1 / num2
+else:
+    result = "Invalid operation"
+
+print(f"{num1} {operation} {num2} = {result}")
+msg = "Week one python module done. God job"
+print(msg)
